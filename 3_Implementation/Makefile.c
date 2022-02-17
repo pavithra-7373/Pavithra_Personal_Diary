a66af59728fd9f44ca578a3fa5b1d58b3ccc08bf
pavithra-7373 <98948425+pavithra-7373@users.noreply.github.com>
2022-02-17 21:32:12 +0530

    Add files via upload

---
+++ 3_Implementation/Makefile.c
@@ -0,0 +1,9 @@
+#target  : Dependencies
+#<TAB>commands
+
+Build : project_main.c personal diary.c test_personal diary. unity.c
+        gcc project_main,c personal diary.c test_personal diary.c unity.c -0 personal diary.out
+Run : personal diary.out
+            ./personal diary.out
+Clean :
+       rm -rf *.0 *.out *.i *.s      
